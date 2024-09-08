df073bf2306b1bb7226e7c2a0b2f2524b313e77f
Siddhartha Maity <siddharthamaity1993@gmail.com>
2024-09-08 18:04:22 +0530

    try

--- Broken/Externals/FFmpeg.py
+++ Broken/Externals/FFmpeg.py
@@ -192,7 +192,7 @@ class FFmpegVideoCodecH264(FFmpegModuleBase):
 
     def command(self, ffmpeg: BrokenFFmpeg) -> Iterable[str]:
         # yield every("-c:v", "libx264rgb" if self.rgb else "libx264")
-        yield every("-c:v", "h264_nvenc")
+        yield every("-c:v", "hevc_nvenc")
         yield every("-profile", self.profile)
         yield every("-preset", self.preset)
         yield every("-tune", self.tune)
